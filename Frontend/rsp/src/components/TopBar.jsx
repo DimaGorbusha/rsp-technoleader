94247b94d19a19d8026847d4a89f5fc550256b3e
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-09-19 16:08:16 +0500

    Edit: улучшена вёрстка

--- Frontend/rsp/src/components/TopBar.jsx
+++ Frontend/rsp/src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import topBarLogo from '../img/topBarLogo.svg'
 
 export default function TopBar(props) {
   return (
-    <div className='flex mt-[11px] align-middle'>
+    <div className='flex my-[11px] align-middle'>
         <img src={topBarLogo} alt="" className='ml-[16px]'/>
         <h1 className='ml-[1094px] text-[14px]'>{ props.name }</h1>
     </div>
